45d63ca8f5de29b15c468f13fc4827f08ebf927e
Dan Morton <dan@purplepenguin.com>
2015-05-20 10:58:56 -0400

    added last build for easy access

---
+++ build/MopubSDK.framework/Versions/A/Headers/MopubSDK.h
@@ -0,0 +1,38 @@
+//
+//  MopubSDK.h
+//  MopubSDK
+//
+//  Created by Dan PurplePenguin on 5/19/15.
+//  Copyright (c) 2015 Mopub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <MopubSDK/MPAdConversionTracker.h>
+#import <MopubSDK/MPAdView.h>
+#import <MopubSDK/MPBannerCustomEvent.h>
+#import <MopubSDK/MPBannerCustomEventDelegate.h>
+#import <MopubSDK/MPConstants.h>
+#import <MopubSDK/MPInterstitialAdController.h>
+#import <MopubSDK/MPInterstitialCustomEvent.h>
+#import <MopubSDK/MPInterstitialCustomEventDelegate.h>
+#import <MopubSDK/MPNativeAd.h>
+#import <MopubSDK/MPNativeAdAdapter.h>
+#import <MopubSDK/MPNativeAdConstants.h>
+#import <MopubSDK/MPNativeCustomEvent.h>
+#import <MopubSDK/MPNativeCustomEventDelegate.h>
+#import <MopubSDK/MPNativeAdError.h>
+#import <MopubSDK/MPNativeAdRendering.h>
+#import <MopubSDK/MPNativeAdRequest.h>
+#import <MopubSDK/MPNativeAdRequestTargeting.h>
+#import <MopubSDK/MPTableViewAdManager.h>
+#import <MopubSDK/MPCollectionViewAdPlacer.h>
+#import <MopubSDK/MPTableViewAdPlacer.h>
+#import <MopubSDK/MPClientAdPositioning.h>
+#import <MopubSDK/MPServerAdPositioning.h>
+#import <MopubSDK/MPNativeAdDelegate.h>
+#import <MopubSDK/MPMediationSettingsProtocol.h>
+#import <MopubSDK/MPRewardedVideo.h>
+#import <MopubSDK/MPRewardedVideoReward.h>
+#import <MopubSDK/MPRewardedVideoCustomEvent.h>
+#import <MopubSDK/MPRewardedVideoError.h>
